d4883c78a27ad59e78714aa0ef5b5b278b8ee63c
Michał Maj <michalmaj116@gmail.com>
2021-08-23 11:14:46 +0200

    Add tests for split_masks_into_binary

--- tests/test_segmentation_generator.py
+++ tests/test_segmentation_generator.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 import pandas as pd
-from platypus.segmentation.generator import create_images_masks_paths
+from platypus.segmentation.generator import create_images_masks_paths, split_masks_into_binary
 
 
 @pytest.mark.parametrize(
@@ -56,3 +56,22 @@ from platypus.segmentation.generator import create_images_masks_paths
 )
 def test_create_images_masks_paths(path, mode, only_images, result):
     assert create_images_masks_paths(path, mode, only_images, ("images", "masks"), ";") == result
+
+
+@pytest.mark.parametrize(
+    "mask, colormap, result",
+    [
+        (np.array([255, 255, 255,
+                   0, 0, 0,
+                   111, 111, 111,
+                   222, 222, 222]).reshape((2, 2, 3)),
+         [(0, 0, 0), (111, 111, 111), (222, 222, 222), (255, 255, 255)],
+         np.array([0, 0, 0, 1,
+                   1, 0, 0, 0,
+                   0, 1, 0, 0,
+                   0, 0, 1, 0]).reshape((2, 2, 4))
+         )
+    ]
+)
+def test_split_masks_into_binary(mask, colormap, result):
+    assert (split_masks_into_binary(mask, colormap) == result).all()
