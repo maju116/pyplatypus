4cc3620f5f38e9321f266d28c419f51931201d41
Michał Maj <michalmaj116@gmail.com>
2022-05-30 18:29:01 +0200

    Fix tests

--- tests/test_segmentation_generator.py
+++ tests/test_segmentation_generator.py
@@ -56,7 +56,7 @@ from platypus.segmentation.generator import *
     ]
 )
 def test_create_images_masks_paths(path, mode, only_images, result):
-    assert create_images_masks_paths(path, mode, only_images, ("images", "masks"), ";") == result
+    assert segmentation_generator.create_images_masks_paths(path, mode, only_images, ("images", "masks"), ";") == result
 
 
 @pytest.mark.parametrize(
@@ -76,96 +76,6 @@ def test_create_images_masks_paths(path, mode, only_images, result):
 )
 def test_split_masks_into_binary(mask, colormap, result):
     assert (split_masks_into_binary(mask, colormap) == result).all()
-
-
-@pytest.mark.parametrize(
-    "paths, indices, scale, colormap, result",
-    [
-        ([['tests/testdata/dir/images/test_image_1.png'],
-          ['tests/testdata/dir/images/test_image_2.png'],
-          ['tests/testdata/dir/images/test_image_3.png']],
-         None, 1 / 255, None,
-         np.stack([
-             np.array([255, 255, 255,
-                       0, 0, 0,
-                       111, 111, 111,
-                       222, 222, 222]).reshape((2, 2, 3)) * (1 / 255),
-             np.array([255, 255, 255,
-                       222, 222, 222,
-                       111, 111, 111,
-                       0, 0, 0]).reshape((2, 2, 3)) * (1 / 255),
-             np.array([222, 222, 222,
-                       0, 0, 0,
-                       111, 111, 111,
-                       255, 255, 255]).reshape((2, 2, 3)) * (1 / 255)
-         ], axis=0)
-         ),
-        ([['tests/testdata/dir/masks/test_mask_1.png'],
-          ['tests/testdata/dir/masks/test_mask_2.png'],
-          ['tests/testdata/dir/masks/test_mask_3.png']],
-         None, 1 / 255, [(0, 0, 0), (111, 111, 111), (222, 222, 222), (255, 255, 255)],
-         np.stack([
-             np.array([0, 0, 0, 1,
-                       1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 1, 0]).reshape((2, 2, 4)),
-             np.array([0, 0, 0, 1,
-                       0, 0, 1, 0,
-                       0, 1, 0, 0,
-                       1, 0, 0, 0]).reshape((2, 2, 4)),
-             np.array([0, 0, 1, 0,
-                       1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 0, 1]).reshape((2, 2, 4))
-         ], axis=0)
-         ),
-        ([['tests/testdata/dir/masks/test_mask_1.png'],
-          ['tests/testdata/dir/masks/test_mask_2.png'],
-          ['tests/testdata/dir/masks/test_mask_3.png']],
-         [0, 1], 1 / 255, [(0, 0, 0), (111, 111, 111), (222, 222, 222), (255, 255, 255)],
-         np.stack([
-             np.array([0, 0, 0, 1,
-                       1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 1, 0]).reshape((2, 2, 4)),
-             np.array([0, 0, 0, 1,
-                       0, 0, 1, 0,
-                       0, 1, 0, 0,
-                       1, 0, 0, 0]).reshape((2, 2, 4))
-         ], axis=0)
-         ),
-        ([['tests/testdata/nested_dirs/image_1/masks/test_mask_1_1.png',
-           'tests/testdata/nested_dirs/image_1/masks/test_mask_1_2.png',
-           'tests/testdata/nested_dirs/image_1/masks/test_mask_1_3.png'],
-          ['tests/testdata/nested_dirs/image_2/masks/test_mask_2_1.png',
-           'tests/testdata/nested_dirs/image_2/masks/test_mask_2_2.png',
-           'tests/testdata/nested_dirs/image_2/masks/test_mask_2_3.png'],
-          ['tests/testdata/nested_dirs/image_3/masks/test_mask_3_1.png',
-           'tests/testdata/nested_dirs/image_3/masks/test_mask_3_2.png',
-           'tests/testdata/nested_dirs/image_3/masks/test_mask_3_3.png']],
-         None, 1 / 255, [(0, 0, 0), (111, 111, 111), (222, 222, 222), (255, 255, 255)],
-         np.stack([
-             np.array([0, 0, 0, 1,
-                       1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 1, 0]).reshape((2, 2, 4)),
-             np.array([0, 0, 0, 1,
-                       0, 0, 1, 0,
-                       0, 1, 0, 0,
-                       1, 0, 0, 0]).reshape((2, 2, 4)),
-             np.array([0, 0, 1, 0,
-                       1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 0, 1]).reshape((2, 2, 4))
-         ], axis=0)
-         )
-    ]
-)
-def test_read_images_from_directory(paths, indices, scale, colormap, result):
-    if colormap is not None:
-        assert (read_images_from_directory(paths, indices, (2, 2), False, scale, colormap) == result).all()
-    else:
-        assert np.allclose(read_images_from_directory(paths, indices, (2, 2), False, scale, colormap), result)
 
 
 @pytest.mark.parametrize(
@@ -205,7 +115,7 @@ def test_read_images_from_directory(paths, indices, scale, colormap, result):
     ]
 )
 def test_segmentation_generator(path, colormap, mode, batch_size, result):
-    test_sg = segmentation_generator(path, colormap, mode, False, 2, 2, False, 1 / 255, batch_size, False,
+    test_sg = segmentation_generator(path, colormap, mode, False, 2, 2, False, None, batch_size, False,
                                      ("images", "masks"), ";")
     output = test_sg.__getitem__(0)
     assert np.allclose(output[0], output[0])
