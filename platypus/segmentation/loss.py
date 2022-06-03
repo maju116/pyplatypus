5c9fbf1d191b95c6c3263ec55a60242979d11a3f
Michał Maj <michalmaj116@gmail.com>
2022-06-03 17:31:17 +0200

    Add IoU metric and loss

--- platypus/segmentation/loss.py
+++ platypus/segmentation/loss.py
@@ -54,3 +54,39 @@ def BCE_dice_loss(
     """
     BCE = kb.binary_crossentropy(tf.cast(kb.sum(y_actual), 'float32'), tf.cast(kb.sum(y_pred), 'float32'))
     return BCE + dice_loss(y_actual, y_pred)
+
+
+def IoU_coefficient(
+        y_actual: tf.Tensor,
+        y_pred: tf.Tensor
+) -> tf.Tensor:
+    """
+    IoU coefficient.
+
+    Args:
+        y_actual (tf.Tensor): True segmentation mask.
+        y_pred (tf.Tensor): Predicted segmentation mask.
+
+    Returns:
+        IoU coefficient.
+    """
+    intersection = tf.cast(kb.sum(y_actual * y_pred), 'float32')
+    union = tf.cast(kb.sum(y_actual) + kb.sum(y_pred), 'float32') - intersection
+    return (intersection + kb.epsilon()) / (union + kb.epsilon())
+
+
+def IoU_loss(
+        y_actual: tf.Tensor,
+        y_pred: tf.Tensor
+) -> tf.Tensor:
+    """
+    IoU loss.
+
+    Args:
+        y_actual (tf.Tensor): True segmentation mask.
+        y_pred (tf.Tensor): Predicted segmentation mask.
+
+    Returns:
+        IoU loss.
+    """
+    return 1 - IoU_coefficient(y_actual, y_pred)
