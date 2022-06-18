f01d5f7cacc9e7dabf8b5e1e0ff4b6e6db740e79
Michał Maj <michalmaj116@gmail.com>
2022-06-18 13:56:58 +0200

    Fix typos in engine

--- platypus/engine.py
+++ platypus/engine.py
@@ -85,8 +85,8 @@ class platypus_engine:
                     dropout=model_cfg['dropout'],
                     batch_normalization=model_cfg['batch_normalization'],
                     kernel_initializer=model_cfg['kernel_initializer'],
-                    plus_plus=model_cfg['kernel_initializer']['plus_plus'],
-                    deep_supervision=model_cfg['kernel_initializer']['deep_supervision']
+                    plus_plus=model_cfg['plus_plus'],
+                    deep_supervision=model_cfg['deep_supervision']
                 ).model
                 # Add options for selection!!!
                 sl = segmentation_loss(n_class=model_cfg['n_class'], background_index=None)
