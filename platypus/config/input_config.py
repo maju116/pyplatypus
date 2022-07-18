3ef8efc339d8344f9331db035fc9b6e2da960fc0
jakubborkowski <borkowski1110@gmail.com>
2022-07-18 15:37:58 +0200

    Model refactored slightly

--- platypus/config/input_config.py
+++ platypus/config/input_config.py
@@ -1,3 +1,2 @@
-implemented_models = ["u_net", "res_u_net"]
 implemented_modes = ["nested_dirs", "config_file"]
 available_activations = ["relu", "sigmoid", "softmax", "softplus", "softsign", "tanh", "selu", "elu", "exponential"]
