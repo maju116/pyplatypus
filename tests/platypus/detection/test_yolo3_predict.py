e0b387cbb352f5d2922a1749ff2d6b9b2f80cc64
Michał Maj <michalmaj116@gmail.com>
2022-08-24 13:59:47 +0200

    Add missing arg

--- tests/platypus/detection/test_yolo3_predict.py
+++ tests/platypus/detection/test_yolo3_predict.py
@@ -14,13 +14,13 @@ test_y3p = yolo3_predict(test_img_preds, coco_anchors, coco_labels, image_h=416,
                          [(0, 0.5), (-0.3, 0.425557483188341),
                           (0.2, 0.549833997312478), (11, 0.999983298578152)])
 def test_sigmoid(x, y):
-    assert test_y3p.sigmoid() == y
+    assert test_y3p.sigmoid(x) == y
 
 
 @pytest.mark.parametrize("x, y",
                          [(0.1, -2.197224577336219), (0.5, 0), (0.77, 1.2083112059245342)])
 def test_logit(x, y):
-    assert test_y3p.logit() == y
+    assert test_y3p.logit(x) == y
 
 
 @pytest.mark.parametrize("predictions, result",
