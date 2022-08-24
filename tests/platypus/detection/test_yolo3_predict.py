a8efe07358020c6dcd61cbf15f696adfb3e8e616
Michał Maj <michalmaj116@gmail.com>
2022-08-24 13:46:29 +0200

    Test get_boxes method

--- tests/platypus/detection/test_yolo3_predict.py
+++ tests/platypus/detection/test_yolo3_predict.py
@@ -21,3 +21,29 @@ def test_sigmoid(x, y):
                          [(0.1, -2.197224577336219), (0.5, 0), (0.77, 1.2083112059245342)])
 def test_logit(x, y):
     assert test_y3p.logit() == y
+
+
+@pytest.mark.parametrize("predictions, result",
+                         (test_img_preds,
+                          [pd.DataFrame({
+                              'xmin': (86, 337, 145, 182, 201, 208, 25, 222),
+                              'ymin': (298, 315, 292, 251, 225, 225, 230, 237),
+                              'xmax': (98, 351, 204, 195, 207, 214, 277, 280),
+                              'ymax': (359, 361, 367, 267, 232, 231, 336, 340),
+                              'p': (0.950761, 0.958715, 0.999735, 0.841772,
+                                    0.837264, 0.820939, 0.999457, 0.954366),
+                              'lebel_id': (0, 0, 2, 2, 2, 2, 5, 6),
+                              'label': ('person', 'person', 'car', 'car', 'car', 'car',
+                                        'bus', 'train')
+                          }),
+                              pd.DataFrame({
+                                  'xmin': (9, 120, 202),
+                                  'ymin': (29, 85, 169),
+                                  'xmax': (188, 303, 352),
+                                  'ymax': (378, 374, 386),
+                                  'p': (0.999947, 0.997230, 0.999533),
+                                  'lebel_id': (22, 22, 22),
+                                  'lebel': ('zebra', 'zebra', 'zebra')
+                              })]))
+def test_get_boxes(predictions, result):
+    assert test_y3p.get_boxes() == result
