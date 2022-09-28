eab040cfbff76c48a51e06b98266b596ad003fa0
jakubborkowski <borkowski1110@gmail.com>
2022-09-28 17:12:20 +0200

    Outputting fix

--- pyplatypus/utils/prediction_utils.py
+++ pyplatypus/utils/prediction_utils.py
@@ -3,15 +3,20 @@ from PIL import Image
 import numpy as np
 
 
-def save_masks(image_masks: list, paths: list, model_name: str):
+def save_masks(image_masks: list, paths: list, model_name: str, mode: str = "nested_dirs"):
     for im, path in zip(image_masks, paths):
-        img_directory = Path(path).parents[1]
-        img_name = Path(path).name
-        masks_path = img_directory/"predicted_masks"
-        Path.mkdir(masks_path, exist_ok=True)
-        img_name, img_type = Path(path).name.split(".")
+        image_path = Path(path)
+        img_name, img_type = image_path.name.split(".")
         if img_type != "png":
             raise NotImplementedError("Types other than PNG to be handled soon.")
+        if mode == "nested_dirs":
+            img_directory = image_path.parents[1]
+            img_name = img_directory.name
+        else:
+            img_directory = image_path.parents[0]
+            img_name = img_directory.name
+        masks_path = img_directory/"predicted_masks"
+        Path.mkdir(masks_path, exist_ok=True)
         mask_as_image = Image.fromarray(im.astype(np.uint8)).convert("RGB")
         mask_path = masks_path/f"{img_name}_{model_name}_predicted_mask.png"
         mask_as_image.save(mask_path)
