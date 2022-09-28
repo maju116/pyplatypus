b336ab17666c20c67dbd5e1fc3b2016f4ecf1f71
jakubborkowski <borkowski1110@gmail.com>
2022-09-28 10:24:57 +0200

    Image name added to the produced predictions

--- pyplatypus/utils/prediction_utils.py
+++ pyplatypus/utils/prediction_utils.py
@@ -6,11 +6,12 @@ import numpy as np
 def save_masks(image_masks: list, paths: list, model_name: str):
     for im, path in zip(image_masks, paths):
         img_directory = Path(path).parents[1]
+        img_name = img_directory.stem
         masks_path = img_directory/"predicted_masks"
         Path.mkdir(masks_path, exist_ok=True)
         img_name, img_type = Path(path).name.split(".")
         if img_type != "png":
             raise NotImplementedError("Types other than PNG to be handled soon.")
         mask_as_image = Image.fromarray(im.astype(np.uint8)).convert("RGB")
-        mask_path = masks_path/f"{model_name}_predicted_mask.png"
+        mask_path = masks_path/f"{img_name}_{model_name}_predicted_mask.png"
         mask_as_image.save(mask_path)
--- tests/platypus/test_end_to_end.py
+++ tests/platypus/test_end_to_end.py
@@ -1,6 +1,7 @@
 from pyplatypus.solvers.platypus_cv_solver import PlatypusSolver
 
-def test_complete_run():
+def test_complete_run(tmpdir):
     pe = PlatypusSolver(config_yaml_path="tests/testdata/config_yaml/end_to_end_testing_config.yaml")
     pe.run()
+    pe.produce_and_save_predicted_masks_for_model(model_name="res_u_net", custom_data_path=tmpdir)
     assert pe.cache.get("semantic_segmentation")
