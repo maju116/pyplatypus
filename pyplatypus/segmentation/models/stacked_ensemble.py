acfc649dbaafb25744ee62955ef529159ba32a6c
maju116 <michalmaj116@gmail.com>
2022-10-05 11:52:11 +0200

    Add copy_submodels method

--- pyplatypus/segmentation/models/stacked_ensemble.py
+++ pyplatypus/segmentation/models/stacked_ensemble.py
@@ -3,6 +3,7 @@ from tensorflow.keras.layers import (
     Concatenate, Cropping2D, Resizing, Average, Add, Conv2D, SpatialDropout2D,
     UpSampling2D
 )
+from tensorflow.keras.models import clone_model
 from tensorflow.keras import activations as KRACT
 from tensorflow.keras.backend import int_shape
 from tensorflow.keras import Model, Input
@@ -14,18 +15,40 @@ class semantic_segmentation_ensembler:
 
     def __init__(
             self,
-            models: List[tf.keras.Model],
+            submodels: List[tf.keras.Model],
+            copy_submodels_weights: bool = True,
+            freeze_submodels_weights: bool = True,
             **kwargs
     ) -> None:
         """Creates semantic segmentation stacked ensemble model architecture.
 
         Parameters
         ----------
-        models : List[tf.keras.Model]
+        submodels : List[tf.keras.Model]
             List with semantic segmentation models.
+        copy_submodels_weights : bool
+            Should the stacked ensembler be initialized with submodels weights.
+        freeze_submodels : bool
+            Should the submodels weights be freezed (only if copy_submodels_weights == `True`).
         """
-        self.models = models
+        self.submodels = submodels
+        self.copy_submodels_weights = copy_submodels_weights
+        self.freeze_submodels_weights = freeze_submodels_weights
+        self.submodels = self.copy_submodels()
         self.model = self.build_model()
+
+    def copy_submodels(self) -> List[tf.Tensor]:
+        """
+        Creates a copy of submodels.
+
+        Returns:
+            List of submodels.
+        """
+        submodels_copy = [clone_model(m) for m in self.submodels]
+        if self.copy_submodels_weights:
+            for mc, m in zip(submodels_copy, self.submodels):
+                mc.set_weights(m.get_weights())
+        return submodels_copy
 
     def build_model(self) -> tf.keras.Model:
         """
@@ -36,13 +59,13 @@ class semantic_segmentation_ensembler:
         model:
             Semantic segmentation stacked ensemble model.
         """
-        for i in range(len(self.models)):
-            model = self.models[i]
+        for i in range(len(self.submodels)):
+            model = self.submodels[i]
             for layer in model.layers:
                 layer.trainable = False
                 layer._name = 'ensemble_' + str(i + 1) + '_' + layer.name
-        ensemble_visible = [model.input for model in self.models]
-        ensemble_outputs = [model.output for model in self.models]
+        ensemble_visible = [model.input for model in self.submodels]
+        ensemble_outputs = [model.output for model in self.submodels]
         # ToDo: reshape outputs to the same HxW
         merge = Concatenate()(ensemble_outputs)
         # ToDo: Add Conv2d and Pool2d
