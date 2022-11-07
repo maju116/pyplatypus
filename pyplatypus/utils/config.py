2c3871dbc5d4e6574b9d500fb8e64ae91620d74d
Michał Maj <michalmaj116@gmail.com>
2022-11-07 19:54:44 +0100

    Update pyplatypus/utils/config.py
    Co-authored-by: Jakub Borkowski <49642462+borkowski1110@users.noreply.github.com>

--- pyplatypus/utils/config.py
+++ pyplatypus/utils/config.py
@@ -123,7 +123,7 @@ class YamlConfigLoader(object):
         Parameters
         ----------
         model_config : dict
-            The element of the list of models from the input constants.
+            The element of the list of models from the input config.
 
         Returns
         -------
